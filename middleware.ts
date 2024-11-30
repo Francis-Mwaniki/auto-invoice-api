a5a794b5db9953bfaff1688cd525b7d4a88eb7b5
Franc-dev <francismwanik254@gmail.com>
2024-11-30 15:25:41 +0300

    added env locals and  generate api

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)', 
   '/sign-up(.*)',
+  '/',
   '/api/generate-invoice' // Add the specific invoice generation API route
 ])
 
